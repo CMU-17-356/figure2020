6fcf12c7014479b5716f8f4ae91d9e2b263a8c74
Tianyi Zhu <yitian9898@gmail.com>
2020-04-12 17:58:59 -0400

    added the questionResponse page with Google Chart

--- client/src/components/response/response.js
+++ client/src/components/response/response.js
@@ -1,18 +1,47 @@
 import React, {Component} from "react";
+import {Chart} from "react-google-charts";
+import {Card} from "react-bootstrap";
 
+let questionTitle = 'How many hours did you sleep last night?';
 
 /*************************/
 /*  Class Implementation */
 /*************************/
 export class Response extends Component {
-  constructor() {
-    super();
+  constructor(props) {
+    super(props);
     this.state = {
     }
   }
 
+
   render() {
-    return ("hello world")
+    return (
+      <div className="container mx-auto">
+        <Card>
+          <Card.Body>
+            <h2>{questionTitle}</h2>
+          </Card.Body>
+        </Card>
+        <div className="my-2">
+          <Chart
+            height={500}
+            chartType="ColumnChart"
+            loader={<div>Loading Chart</div>}
+            data={[
+              ['Option', 'Number of Response'],
+              ['< 3', 12],
+              ['3-6', 15],
+              ['6-9', 13],
+              ['> 9', 11],
+            ]}
+            options={{
+              titlePosition: 'none'
+            }}
+          />
+        </div>
+      </div>
+    )
 
 
   }
