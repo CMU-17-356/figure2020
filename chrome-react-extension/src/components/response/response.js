62578a775d446b6a44e9957e3c9e824af2a38c03
Vrinda Gupta <vrindag@andrew.cmu.edu>
2020-04-15 23:56:57 -0400

    fixed charting by switching to chart js on client side

--- chrome-react-extension/src/components/response/response.js
+++ chrome-react-extension/src/components/response/response.js
@@ -1,8 +1,9 @@
 import React, {Component} from "react";
-import {Chart} from "react-google-charts";
 import {Card} from "react-bootstrap";
 import '../../App.css';
 import './response.css';
+import {Bar} from 'react-chartjs-2';
+
 
 
 let questionTitle = 'How many hours did you sleep last night?';
@@ -10,7 +11,26 @@ let questionTitle = 'How many hours did you sleep last night?';
 /*************************/
 /*  Class Implementation */
 /*************************/
-const google = window.google;
+
+const legendOpts = {
+  display: false
+};
+
+const data = {
+  labels: ['Less than 3', '3 to 6', '6 to 9', 'More than 9'],
+  datasets: [
+    {
+      label: 'Number',
+      backgroundColor: 'rgba(0,0,0,0.5)',
+      borderColor: 'rgba(0,0,0,1)',
+      borderWidth: 2,
+      hoverBackgroundColor: 'rgba(245,245,245,0.4)',
+      hoverBorderColor: 'rgba(0,0,0,1)',
+      data: [15, 20, 100, 57]
+    }
+  ],
+  legend: false
+};
 
 export class Response extends Component {
   constructor(props) {
@@ -26,36 +46,24 @@ export class Response extends Component {
           Hello, friend! <br></br>
         </div>
         <br></br>
-        <Card style={{"background-color": "rgba(245, 245, 245, .5)", "borderStyle": "solid", "borderWidth": "1px", "borderColor": "#FFF4F9", "border-radius": "15px", "width": "400px"}}>
+        <Card style={{"background-color": "rgba(245, 245, 245, .5)", "borderStyle": "solid", "borderWidth": "1px", "borderColor": "#FFF4F9", "border-radius": "15px", "width": "550px"}}>
         <div style={{ "font-size": "25px", "font-weight": "900px", "text-shadow": "3px 3px #D3D3D3", display: "flex", "flexDirection": "row", "justifyContent": "center", "text-align": "center"}}>
           <br></br>Hours slept last night
         </div>
+        <div style={{"margin":"30px"}}>
+        <Bar
+          legend={legendOpts}
+          data={data}
+          width={100}
+          height={50}
+          options={{
+            maintainAspectRatio: true
+          }}
+        />
+      </div>
 
-        <div className="my-2">
-          <Chart 
-            height={350}
-            width = {400}
-            chartType="ColumnChart"
-            loader={<div>Loading Chart</div>}
-            data={[
-              ['Option', 'Number of Responses', { role: 'style' }],
-              ['Less than 3 hours', 12, "color:black;fill-opacity:0.3;stroke-width: 2;"],
-              ['3 to 6 hours', 15, "color:black;fill-opacity:0.3;stroke-width: 2;"],
-              ['6 to 9 hours', 13, "color:black;fill-opacity:0.3;stroke-width: 2;"],
-              ['More than 9 hours', 11, "color:black;fill-opacity:0.3;stroke-width: 2;"],
-            ]}
-            options={{
-              "backgroundColor": "transparent",
-              "color": "black",
-              legend: { position: "none" },
-              vAxis: {
-                gridlines: {
-                color: 'transparent',
-                }
-              }
-            }}
-          />
-        </div>
+        <hr style={{"position": "relative", "top": "250px", "margin-right":"50px","margin-left":"50px"}}></hr>
+
         </Card>
       </div>
     )
